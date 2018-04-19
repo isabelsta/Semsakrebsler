322037252817236b9cc3efe08b69c355a2261e9f
isabelsta <isabel.staaden@t-online.de>
2018-04-19 10:46:28 +0200

    kunden

--- request.js
+++ request.js
@@ -46,9 +46,21 @@ function showWine(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-           document.getElementById("h4").innerHTML = this.responseText.list[0].Name;
+           document.getElementById("name").innerHTML = this.responseText.list[0].Name;
+           document.getElementById("jahr").innerHTML = this.responseText.list[0].Jahr;
         }
     };
     xhttp.open("GET", "http://localhost:8080/rest/api/getWines", true);
+    xhttp.send();
+}
+
+function showLieferanten(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+           document.getElementById("h4").innerHTML = this.responseText.list[0].Name;
+        }
+    };
+    xhttp.open("GET", "http://localhost:8080/rest/api/", true);
     xhttp.send();
 }
